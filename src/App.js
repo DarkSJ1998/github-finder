fcac8cdacbf992350337356c6f805010bc91f2d8
DarkSJ1998 <sarthakjn12@gmail.com>
2020-07-22 17:25:30 +0530

    Implemented Routes for Home and About

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -38,20 +40,37 @@ class App extends Component {
 		const { users, loading } = this.state;
 
 		return (
-			<div className='App'>
-				<NavBar />
-				<div className='container'>
-					{this.state.alert && <Alert alert={this.state.alert} />}
-
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-						setAlert={this.setAlert}
-					/>
-					<Users loading={loading} users={users} />
+			<Router>
+				<div className='App'>
+					<NavBar />
+					<div className='container'>
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={(props) => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={
+												users.length > 0 ? true : false
+											}
+											setAlert={this.setAlert}
+										/>
+										<Users
+											loading={loading}
+											users={users}
+										/>
+									</Fragment>
+								)}
+							/>
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
