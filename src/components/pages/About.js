283b6b79ff7a1b55c206ff809276c45c982ecaf4
DarkSJ1998 <sarthakjn12@gmail.com>
2020-07-26 19:08:36 +0530

    Updated About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,8 @@ const About = () => {
 			<h1>About This App</h1>
 			<p>App to search Github users</p>
 			<p>Version: 1.0.0</p>
+			<p>Created by: DarkSJ1998</p>
+			<p>Based on a tutorial by Brad Traversy</p>
 		</Fragment>
 	);
 };
